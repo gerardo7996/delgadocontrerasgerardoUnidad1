0be3e1a808095c5d5640a1e11612c6113a7cd2e9
gerardo7996 <126217775+gerardo7996@users.noreply.github.com>
2023-05-25 14:34:06 -0600

    Add files via upload

---
+++ chatbot.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Metas del sitio -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Título del sitio -->
+	<title>Chatbot del sitio</title>
+	<!-- Favicon del sitio -->
+	<link rel="icon" href="img/chatbot.png" alt="favicon">
+	<!-- Bootstrap 5.3.0 CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<!-- Bootstrap 5.3.0 JavaScript -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+	<!-- Bootstrap 1.10.5 CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+	<!-- Script de Tidio para el chatbot -->
+	<script src="//code.tidio.co/0yrnqj0mvt7wyticvjxpefkzxvycdtzt.js" async></script>
+	<!-- Estilos del sitio -->
+	<style type="text/css">
+		* {
+			margin: 0;
+		}
+
+		body {
+			margin: 0;
+			padding: 0;
+			background-color: #84FFFF;
+		}
+
+		.navbar {
+			background-color: #0091EA;
+		}
+
+		.navbar h5 {
+			font-size: 26px;
+			color: whitesmoke;
+			margin-left: 25px;
+		}
+
+		h4 {
+			font-size: 20px;
+			color: black;
+		}
+
+		h5 {
+			font-size: 20px;
+			color: black;
+		}
+
+		.container {
+			border-style: groove;
+			border-color: #004D40;
+			background-color: #0091EA;
+			border-radius: 12px;
+		}
+
+		.btn {
+			background-color: #1A237E;
+			color: whitesmoke;
+			font-size: 18px;
+			border-radius: 5%;
+		}
+
+		.btn:hover {
+			background-color: #00E5FF;
+			cursor: pointer;
+			color: black;
+		}
+
+		.icono-chatbot {
+			width: 200px;
+		}
+	</style>
+</head>
+<body class="sb-nav-fixed">
+	<nav class="navbar">
+		<h5 class="navbar-brand"><strong>Mi tienda web</strong></h5>
+		<a class="d-flex btn" href="inicio.php" style="margin-right: 25px;"><i class="bi bi-backspace" style="margin-right: 7px;"></i><strong>Regresar</strong></a>
+	</nav>
+	<!-- Contenedor del apartado -->
+	<div class="container mt-5">
+		<br>
+		<center><img class="icono-chatbot" src="img/chatbot-icon.png"></center>
+		<h4 class="mt-5" align="center"><strong>Interactua con nuestro chat en línea</strong></h4><hr>
+		<h5 align="center">Para todos nuestros clientes que lleguen a tener dudas, pueden comentar con nuestro Chat Bot en línea de nuestro sitio para que puedan resolver sus dudas.<br><br>En la parte inferior de la derecha de esta sección puede encontrar el chat de nuestro sitio web para cualquier duda que lleguen a tener.</h5><br>
+	</div>
+</body>
+</html>
